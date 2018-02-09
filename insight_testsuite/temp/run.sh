b54ad788a6381a5fdd915a57306c97125e5cf592
Nadav Tanners <nadavtanners@gmail.com>
2018-02-09 17:23:00 -0500

    Update run.sh

--- insight_testsuite/temp/run.sh
+++ insight_testsuite/temp/run.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-set -x
 #Execute the following command from the root directory
 python ./src/donation_analytics.py ./input/percentile.txt ./input/itcont.txt ./output/repeat_donors.txt
-
-read
-set +x
